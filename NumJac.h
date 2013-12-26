0d2f5d141aaf58442fe4ca878dc3f1c753670c8f
Administrator <fneuhaus@uni-koblenz.de>
2013-12-26 17:48:25 +0100

    numerical jacobian

---
+++ NumJac.h
@@ -0,0 +1,30 @@
+#include <Eigen/Core>
+#include <stdexcept>
+
+template<typename T, typename P>
+auto numJacobian(T fn, const P& x) -> Eigen::Matrix<double,decltype(fn(x))::RowsAtCompileTime,P::RowsAtCompileTime>
+{
+	const int Rows=decltype(fn(x))::RowsAtCompileTime;
+	Eigen::Matrix<double,Rows,P::RowsAtCompileTime> jac;
+
+	const double eps=0.0001;
+	auto xh=x;
+	for (int i=0;i<x.rows();i++)
+	{
+		xh(i)=x(i)+eps;
+		const auto a=fn(xh);
+		xh(i)=x(i)-eps;
+		const auto b=fn(xh);
+
+		if (Rows==Eigen::Dynamic)
+		{
+			if (i==0)
+				jac.resize(a.rows(),x.rows());
+			if ((a.rows()!=jac.rows())||(b.rows()!=jac.rows()))
+				throw std::runtime_error("Dynamic Dimensions do not match!");
+		}
+
+		jac.col(i)=(a-b)/(2.0*eps);
+	}
+	return jac;
+}
